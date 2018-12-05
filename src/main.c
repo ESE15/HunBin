e59fd10749a87843ce3464eb6811e2ab5f58a01e
ESE15 <junghoon450@gmail.com>
2018-12-05 20:55:14 +0900

    Init src/main.c, clean prj_dir

--- src/main.c
+++ src/main.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "JH_SEARCH.h"
+#include "YB_SCRIPT.h"
 
+int main(int argc, char *argv[]){
+
+
+	// arg parsing
+
+
+	// do search
+	SearchDependencies(
+
+	// do script
+	// YB's code here
+
+	return 0;
+}
