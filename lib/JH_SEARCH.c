bfd54c5eaa71026a9afa0ae2d80f882cca0ace50
ESE15 <junghoon450@gmail.com>
2018-12-05 23:44:33 +0900

    JH_SEARCH.c edit, to be continued

--- lib/JH_SEARCH.c
+++ lib/JH_SEARCH.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
 #include "JH_SEARCH.h"
 
-void SearchDependencies(char *dir){
+DIR *dirPointer;
+struct dirent *dp;
+struct dirent *include;
+struct dirent *library;
+struct dirent *src;
+struct stat statBuffer;
+int fileStat;
+
+int SearchDependencies(char *dir){
 	
+
+	// open Directory ( Project directory )
+	dirPointer = opendir(dir);
+	if(dirPointer == NULL){
+		perror("opendir");
+		exit(0);
+	}
+
+	// read Directory ( Porject directory)
+	while(dp = readdir(dirp)){
+		if(dp==NULL){
+			break;
+		}
+		fileStat = stat(dp->d_name, &statBuffer);
+		if(fileStat==-1){
+			perror("stat");
+			exit(0);
+		}
+		//if( (statBuffer.st_mode & S_IFMT) == S_IFREG){
+		//}
+		if( (statBuffer.st_mode & S_IFMT) == S_IFDIR){
+			// check if there are reserved directories
+			if(strcmp(dp->d_name, "include")== 0){
+			}
+			else if {} // ...
+		}
+		//else if( (statBuffer.st_mode & S_IFMT) == S_IFLNK){
+		//}
+	}
+
+	// return status  0: error , 1 : done
 }
