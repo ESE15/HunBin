a7bf44d7254437adf114fb2c017443b72c78d9b9
정영빈 <>
2018-12-06 00:48:19 +0900

    script / prototype.txt ,

--- lib/YB_SCRIPT.c
+++ lib/YB_SCRIPT.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define prototypeFileName "prototype.txt"
+
+char buffer[255];
 
 int ScriptMakefile( void ){
-  
+    FILE* pPrototype = NULL;
+    FILE* pMakeFile = NULL;
+    
+    char libName[30];
+    char* headerName[255];
+    
+    if ( fopen(prototypeFileName,"r") == NULL ){
+        perror("cannot find prototype file\n");
+        exit(1);
+    }
+    
+    memset(buffer,"\0",sizeof(buffer)*sizeof(char));
+    
+    while(fgets(buffer,pPrototype)){
+        
+    }
+    
 }
