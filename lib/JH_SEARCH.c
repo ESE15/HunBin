1f4cbf56795e1e7b14e3e572bb58d75760ae5908
ESE15 <junghoon450@gmail.com>
2018-12-09 00:28:04 +0900

    Working directory error modified! ( by adding chdir() )

--- lib/JH_SEARCH.c
+++ lib/JH_SEARCH.c
@@ -4,19 +4,27 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "JH_SEARCH.h"
 
-DIR *dirPointer;
-struct dirent *dp;
-struct dirent *include;
-struct dirent *library;
-struct dirent *src;
-struct stat statBuffer;
-int fileStat;
+//DIR *dirPointer;
+//struct dirent *dp;
+//struct dirent *include;
+//struct dirent *library;
+//struct dirent *src;
+//struct stat statBuffer;
+//int fileStat;
+
 
 int SearchDependencies(char *dir){
-	
-
+	DIR *dirPointer;	
+	struct dirent *dp;
+	struct dirent *include;
+	struct dirent *library;
+	struct dirent *src;
+    struct stat statBuffer;
+	int fileStat;
+	int change_dir;
 	// open Directory ( Project directory )
 	dirPointer = opendir(dir);
 	if(dirPointer == NULL){
@@ -24,18 +32,23 @@ int SearchDependencies(char *dir){
 		exit(0);
 	}
 
+	// Change current directory to working directory
+	// It's NECESSARY
+	change_dir = chdir(dir);
+
 	// read Directory ( Porject directory)
 	while((dp = readdir(dirPointer))!= NULL){
 		if(dp==NULL){
 			break;
 		}
-		printf(" JH_SEARCH.c 's 32 Line \n");
-		printf(" now directory is %s \n",dir);
+//		printf(" JH_SEARCH.c 's 32 Line \n");
+//		printf(" now directory is %s \n",dir);
 		fileStat = stat(dp->d_name, &statBuffer);
-		//if(fileStat==-1){
-		//	perror("stat");
-		//	exit(0);
-		//}
+		if(fileStat==-1){
+			perror("stat");
+			exit(0);
+		}
+		printf(" Now on %s \n",dp->d_name);
 		//if( (statBuffer.st_mode & S_IFMT) == S_IFREG){
 		//}
 		if( (statBuffer.st_mode & S_IFMT) == S_IFDIR){
@@ -44,15 +57,15 @@ int SearchDependencies(char *dir){
 			if(strcmp(dp->d_name, "include") == 0){ // found include directory
 				// do some
 				//SearchInclude(dp->d_name);
-				printf("%s \n",dp->d_name);
+				printf("include : %s \n",dp->d_name);
 			} 
 			else if (strcmp(dp->d_name, "lib") == 0) { // found lib directory
-				printf("%s \n",dp->d_name);
+				printf("lib : %s \n",dp->d_name);
 				// do some
 			} 
 			else if (strcmp(dp->d_name, "src") == 0){ // found src directory
 				// do some
-				printf("%s\n",dp->d_name);
+				printf("src : %s\n",dp->d_name);
 			}
 		}
 		//else if( (statBuffer.st_mode & S_IFMT) == S_IFLNK){
