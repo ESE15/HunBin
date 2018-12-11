fbaf31a012696e15d6a04caaca8d489ba21344e2
정영빈 <>
2018-12-11 11:05:23 +0900

    all complete

--- lib/YB_SCRIPT.c
+++ lib/YB_SCRIPT.c
@@ -14,17 +14,16 @@
 
 char buffer[255];
 
-void parseObjectName(char ** pHeaderName,int headerCnt,int flag){
+char* parseObjectName(char ** pHeaderName,int headerCnt,int flag){
     char temp[255];
     char *temp2;
-    char * tempHeaderName[255];
-    memcpy(tempHeaderName,pHeaderName,sizeof(char*)*255);
-    printf("%d\n",headerCnt);
+    static char tempHeaderName[255];
+    //printf("%d\n",headerCnt);
     if (flag == 1){
         for (int i = 0; i < headerCnt; i++){
             strtok(pHeaderName[i],".");
             strcat(pHeaderName[i],".o");
-            printf("%s\n",pHeaderName[i]);
+            //printf("%s\n",pHeaderName[i]);
         }
     
         for (int i = 0;i < headerCnt-1; i++){
@@ -32,29 +31,18 @@ void parseObjectName(char ** pHeaderName,int headerCnt,int flag){
             sprintf(temp," %s",pHeaderName[i+1]);
             strcat(pHeaderName[0],temp);
         }
+        //printf("OBJS = %s\n",pHeaderName[0]);
     }
     else if (flag == 0){
+        strcpy(tempHeaderName,pHeaderName[0]);
         for (int i = 0;i < headerCnt-1; i++){
             memset(temp,0,255);
             sprintf(temp," %s",pHeaderName[i+1]);
-            strcat(pHeaderName[0],temp);
+            strcat(tempHeaderName,temp);
         }
+        //printf("header list = %s\n",tempHeaderName);
     }
-    else if (flag == 2){
-        for (int i = 0; i < headerCnt; i++){
-            temp2 = pHeaderName[i];
-            sprintf(tempHeaderName[i],"%s%s",INCLUDE_PATH,temp2);
-            printf("%s\n",tempHeaderName[i]);
-        }
-        
-        for (int i = 0;i < headerCnt-1; i++){
-            memset(temp,0,255);
-            sprintf(temp," %s",tempHeaderName[i+1]);
-            strcat(tempHeaderName[0],temp);
-        }
-        memcpy(pHeaderName,tempHeaderName,sizeof(char*)*255);
-        //printf("%s\n",pHeaderName[0]);
-    }
+    return tempHeaderName;
 }
 
 
@@ -62,6 +50,7 @@ int ScriptMakefile( void ){
     int returnValue = FAIL;
     int headerNameIndex = 0;
     int cFileNameIndex = 0;
+    int srcHeaderNameIndex = 0;
     
     FILE* pPrototype = NULL;
     FILE* pMakeFile = NULL;
@@ -69,13 +58,15 @@ int ScriptMakefile( void ){
     FILE* pWholeMakeFile = NULL;
     
     char* path;
-    char* libName;
+    char* Headerlist;
+    char* srcHeaderlist;
     char* headerName[255];
     char* tempHeaderName[255];
     char ObjectName[255];
     char* cFileName[255];
     char* tempPtr;
     char libNameMacroTmp[255];
+    char* srcHeaderName[255];
     
     if ( ( pMainMakeFile = fopen(MainMakeFileName,"w") ) == NULL ){
         perror("cannot create Main Makefile\n");
@@ -104,37 +95,62 @@ int ScriptMakefile( void ){
             tempPtr = buffer;
             strtok(tempPtr,",");
             path = tempPtr;
-            
-            tempPtr = strtok(NULL,",");
-            cFileName[cFileNameIndex] = tempPtr;
-            while( (tempPtr = strtok(NULL,",")) != NULL ){
-                headerName[headerNameIndex] = tempPtr;
-                headerNameIndex++;
+            //printf("path : %s\n",path);
+            if (strcmp(path,"src")==0){
+                tempPtr = strtok(NULL,",");
+                cFileName[cFileNameIndex++] = tempPtr;
+                while( (tempPtr = strtok(NULL,",")) != NULL ){
+                    srcHeaderName[srcHeaderNameIndex] = tempPtr;
+                    srcHeaderNameIndex++;
+                    //printf("src dir headerfile : %s\n",tempPtr);
+                }
+            }
+            else{
+                tempPtr = strtok(NULL,",");
+                cFileName[cFileNameIndex] = tempPtr;
+                cFileNameIndex++;
+                while( (tempPtr = strtok(NULL,",")) != NULL ){
+                    headerName[headerNameIndex] = tempPtr;
+                    headerNameIndex++;
+                }
             }
         }
     }
-    //memcpy(tempHeaderName,headerName,sizeof(char*)*255);
-    //parseObjectName(tempHeaderName,headerNameIndex,0);
     
+    
+    
+    memcpy(tempHeaderName,headerName,sizeof(char*)*255);
+    Headerlist = parseObjectName(tempHeaderName,headerNameIndex,0);
     /* script main Makefile*/
     fputs("CC=gcc\nINCLUDE=-I../include\nLIB=-L../lib\nLIBNAME=-lmyfunc\nOBJS=main.o\nOUTPUT=main\nCFLAGS=-Wall\nHEADERS=",pMainMakeFile);
     
-    fputs(tempHeaderName[0],pMainMakeFile);
+    fputs(Headerlist,pMainMakeFile);
     fputs("\n\n",pMainMakeFile);
           
-    fputs("all : $(OUTPUT)\n\n$(OUTPUT):$(OBJS)\n\t$(CC) -o $(OUTPUT) $(OBJS) $(LIB) -lmyfuncs\n\n",pMainMakeFile);
+    fputs("all : $(OUTPUT)\n\n$(OUTPUT):$(OBJS)\n\t$(CC) -o $(OUTPUT) $(OBJS) $(LIB) -lmyfuncs\n\n%.o: %.c",pMainMakeFile);
     
-    memcpy(tempHeaderName,headerName,sizeof(char*)*255);
-    parseObjectName(tempHeaderName,headerNameIndex,2);
+    for (int i = 0;i<headerNameIndex; i++){
+        if ( i == headerNameIndex-1){
+            headerName[i][strlen(headerName[i])-1] = '\0';
+        }
+        fprintf(pMainMakeFile,"../include/%s ",headerName[i]);
+    }
+    for (int i = 0; i < srcHeaderNameIndex; i++){
+        fprintf(pMainMakeFile,"../include/%s ",srcHeaderName[i]);
+    }
+    fprintf(pMainMakeFile,"\n");
+    fputs("\t$(CC) -c $(CFLAGS) $< $(INCLUDE) -o $@\n\nclean:\n",pMainMakeFile);
+    fputs("\t rm -f $(OUTPUT) $(OBJS)",pMainMakeFile);
+    
     
     /* script whole Makefile*/
     fputs("DIRS=lib src\n.PHONY: all clean\nMAKE=make\nall:\n\t@for d in $(DIRS);\\\n\tdo\\\n\t\t$(MAKE) -C $$d;\\\n\tdone\n\nclean:\n\t@for d in $(DIRS);\\\n\tdo\\\n\t\t$(MAKE) -C $$d clean;\\\n\tdone",pWholeMakeFile);
     
-    //memcpy(tempHeaderName,headerName,sizeof(char*)*255);
-    //parseObjectName(tempHeaderName,headerNameIndex,1);
+    memcpy(tempHeaderName,headerName,sizeof(char*)*255);
+    parseObjectName(tempHeaderName,headerNameIndex,1);
     /* script lib Makefile*/
     fputs("CC=gcc\nCFLAGS=-Wall\nOUTPUT=libmyfunc.a\nINCLUDE=-I../include\n",pMakeFile);
-    fputs("OBJS ~~~~\n\n",pMakeFile);
+    fprintf(pMakeFile,"OBJS=%s\n",tempHeaderName[0]);
     fputs("all : $(OBJS) $(OUTPUT)\n\n$(OUTPUT): $(OBJS)\n\tar rv $(OUTPUT) $(OBJS)\n\n",pMakeFile);
     fputs("%.o: %.c ../include/%.h\n\t$(CC) -c $(CFLAGS) $< $(INCLUDE) -o $@\n\n",pMakeFile);
     fputs("clean:\n\trm -f $(OBJS) $(OUTPUT)\n",pMakeFile);
