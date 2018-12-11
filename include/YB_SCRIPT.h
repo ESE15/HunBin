ac75fc046e64c871bdbec6057afae2f343d023c9
정영빈 <>
2018-12-11 12:20:15 +0900

    header modify

--- include/YB_SCRIPT.h
+++ include/YB_SCRIPT.h
@@ -1,8 +1,8 @@
 #ifndef __YB_SCRIPT_H__
 #define __YB_SCRIPT_H__
 
-
-int ScriptMakefile( void );
+char* parseObjectName(char ** pHeaderName,int headerCnt,int flag);
+int ScriptMakefile( char* );
 
 
 #endif
